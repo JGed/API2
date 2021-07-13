0f002f52b49fe0ab0c19d9d323eaf7146be4996d
Josh Geddes <josh.geddes96@gmail.com>
2021-07-13 19:25:08 -0400

    styling changes

--- apod.js
+++ apod.js
@@ -1,26 +1,20 @@
-import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
-
 fetch("https://api.nasa.gov/planetary/apod?api_key=JRRdWsGr38G2srRNPTYeeyPqeKwJ3h6yrkWEu2pG")
 .then(response => response.json())
 .then(json =>  {
     let container = document.getElementById("apod-container");
-    let element = document.createElement("embed");
+    let element;
     
-    /*
     if(json.media_type === "image"){
         element = document.createElement("img");
         element.src = json.url;
     }
     else {
-        element = document.createElement("video");
-        let source = document.createElement("source");
-        source.src = json.url;
-        element.style.height = "500px";
-        element.style.width = "500px";
+        element = document.createElement("iframe");
+        element.src = json.url;
+        element.style.height = "100%";
+        element.style.width = "100%";
         element.style.frameborder = "0";
-        element.appendChild(source);
     }
-    */
     container.appendChild(element);
 })
 .catch(error => {
